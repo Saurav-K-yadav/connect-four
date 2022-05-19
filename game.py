d758be251772fc1196e433cd6f2a2893246ea1f3
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2022-05-19 22:36:47 +0530

    simple connect 4 game is ready

--- game.py
+++ game.py
@@ -33,6 +33,52 @@ def print_board(board):  # To print the game board
     print(np.flip(board, 0))  # flip the board along 0
 
 
+def win(board, piece):  # check if the player is winning
+    # checking horizontolly
+    for cols in range(col_count - 3):  # last three can't make horizontal pattern
+        for rows in range(row_count):
+            if (
+                (board[rows][cols] == piece)
+                and (board[rows][cols + 1] == piece)
+                and (board[rows][cols + 2] == piece)
+                and (board[rows][cols + 3] == piece)
+            ):
+                return True
+
+    # Checking Vertically
+    for cols in range(col_count):
+        for rows in range(row_count - 3):  # Top ones can't make the pattern
+            if (
+                (board[rows][cols] == piece)
+                and (board[rows + 1][cols] == piece)
+                and (board[rows + 2][cols] == piece)
+                and (board[rows + 3][cols] == piece)
+            ):
+                return True
+
+    # For Diagnols
+    # For going upwards
+    for cols in range(col_count - 3):  # right ones can't make the pattern
+        for rows in range(row_count - 3):  # Top ones can't make the pattern
+            if (
+                (board[rows][cols] == piece)
+                and (board[rows + 1][cols + 1] == piece)
+                and (board[rows + 2][cols + 2] == piece)
+                and (board[rows + 3][cols + 3] == piece)
+            ):
+                return True
+    # For diagnolly downwards
+    for cols in range(col_count - 3):  # right ones can't make the pattern
+        for rows in range(3, row_count):  # bottom ones can't make the pattern
+            if (
+                (board[rows][cols] == piece)
+                and (board[rows - 1][cols + 1] == piece)
+                and (board[rows - 2][cols + 2] == piece)
+                and (board[rows - 3][cols + 3] == piece)
+            ):
+                return True
+
+
 board = board()  # A 6*7 matrix with all zeros
 game_over = False
 turn = 0  # player 1 goes first
@@ -44,13 +90,22 @@ while not game_over:
         if can_put(board, col_move):
             row = next_top_row(board, col_move)
             put_piece(board, row, col_move, 1)  # player 1's piece is 1
+        print_board(board)
+
+        if win(board, 1):
+            print("!!!! Player 1 wins !!!")
+            game_over = True  # The game is over
 
     else:  # Player 2's turn
         col_move = int(input(" Player 2's turn "))
         if can_put(board, col_move):
             row = next_top_row(board, col_move)
             put_piece(board, row, col_move, 2)  # player 2's piece is 2
+        print_board(board)
 
-    print_board(board)
+        if win(board, 2):
+            print("!!! Player 2 wins !!!")
+            game_over = True  # The game is over
+
     turn += 1
     turn = turn % 2  # Turn we either be 0 or 1
