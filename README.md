c3b173e1b707227ef2e71839da0de9b90db2189a
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2022-05-20 07:16:03 +0530

    The game is complete with min-max algorithm using alpha-beta pruning

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 <p>connect Four is a two-player connection board game, in which the players choose a color and then take turns dropping colored tokens into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own tokens. </p>
 
 <p> This game is made using min-max algorithm with alpha beta prunning</p>
+<h2>library used</h2> 
+<li>--pygame
+            <ul>pip install pygame</ul>
+            </li>
+
+<li>--numpy
+<ul>pip install numpy</ul></li>
--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import random
+from tkinter import scrolledtext
 import numpy as np
 import sys
 import pygame  # For the GUI
@@ -92,7 +94,7 @@ def draw_board(board):  # GUI for the board
             # surface,color,position,dimension
             pygame.draw.circle(
                 screen,
-                (255,255,255),
+                (255, 255, 255),
                 (
                     int(c * SQUARESIZE + SQUARESIZE / 2),
                     int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2),
@@ -135,12 +137,162 @@ def draw_board(board):  # GUI for the board
     pygame.display.update()  # To update the screen
 
 
+def evaluate(window, piece):
+    score = 0
+    opponent = 1
+    if piece == 1:
+        opponent = 2
+
+    if window.count(piece) == 4:
+        score += 100  # If we find 4 pieces in a row
+    elif (window.count(piece) == 3) and window.count(0) == 1:
+        score += 5  # if we find 3 pieces in a row and 1 empty
+    elif window.count(piece) == 2 and window.count(0) == 2:
+        score += 2
+    if (
+        window.count(opponent) == 3 and window.count(0) == 1
+    ):  # if the opponent has three in a row and a blank space
+        score -= 4  # negative if the opponent is winning
+    return score
+
+
+def get_score(board, piece):  # For heuristic
+    # Horizontally
+    score = 0
+    # Giving preference for piece in center
+    center_array = [int(i) for i in list(board[:, col_count // 2])]
+    center_count = center_array.count(piece)
+    score += center_count * 3  # three score for each piece
+
+    for r in range(row_count):
+        # extract each row from the board
+        row_arr = [int(i) for i in list(board[r, :])]
+        for c in range(col_count - 3):  # last 3 can't be horizontal's starting point
+            window = row_arr[c : c + 4]  # take four columns at a time
+            score += evaluate(window, piece)
+
+    # For vetical
+    for c in range(col_count):
+        # extract each column from the board
+        col_arr = [int(i) for i in list(board[:, c])]
+        for c in range(row_count - 3):  # last 3 can't be vertical's starting point
+            window = col_arr[c : c + 4]  # take four columns at a time
+            score += evaluate(window, piece)
+
+    # For slopes
+    # slope going diagnolly up
+    for r in range(row_count - 3):
+        for c in range(col_count - 3):
+            window = [
+                board[r + i][c + i] for i in range(4)
+            ]  # taking the board's diagnol elements
+            score += evaluate(window, piece)
+
+    for r in range(row_count - 3):
+        for c in range(col_count - 3):
+            window = [
+                board[r + 3 - i][c + i] for i in range(4)
+            ]  # for storing diagnolly downwards
+            # +3 as we can't start from last three
+            # row decrease but column increases
+            score += evaluate(window, piece)
+
+    return score
+
+
+def is_terminal(board):  # check if the end is achieved
+    return win(board, 1) or win(board, 2) or len(get_valid_location(board == 0))
+
+
+# A node is a terminal lnode if it's either one of player wins or there is no possible moves remaining
+
+
+def minmax(board, depth, alpha, beta, maximizing):  # Min-Max algorithm
+    valid = get_valid_location(board)
+    terminal = is_terminal(board)
+    if depth == 0 or terminal:
+        if terminal:  # if the game gets over
+            if win(board, 2):
+                return (None, 1000000000000)  # if the AI wins return a very big number
+            elif win(board, 1):
+                return (
+                    None,
+                    1000000000000,
+                )  # if human wins return a vey lage negative number
+            else:
+                return (None, 0)  # if out of moves return 0
+        else:  # If the depth is zero
+            return None, get_score(
+                board, 2
+            )  # if we reached the final depth return the score
+    if maximizing:  # For maximizing half
+        value = -math.inf  # taking initial value as negative infinity
+        column = random.choice(valid)  # choosing one of valid locagions randomly
+        for col in valid:
+            row = next_top_row(board, col)
+            b_copy = board.copy()
+            put_piece(b_copy, row, col, 2)
+            new_score = minmax(b_copy, depth - 1, alpha, beta, False)[
+                1
+            ]  # calculating the score recurively
+            if new_score > value:
+                value = new_score  # updating hte value
+                column = col
+            alpha = max(alpha, value)
+            if alpha >= beta:  # Prune the branch
+                break
+
+        return column, value  # returning the max's column,the score at it
+
+    else:  # Minimizing half
+        value = math.inf  # Taking positive infinity
+        for col in valid:
+            row = next_top_row(board, col)
+            b_copy = board.copy()
+            put_piece(b_copy, row, col, 1)
+            new_score = minmax(b_copy, depth - 1, alpha, beta, True)[
+                1
+            ]  # finding value recursively
+            if new_score < value:
+                value = new_score
+                column = col
+            beta = min(beta, value)
+            if alpha >= beta:
+                break  # Prune
+        return column, value  # returning column and value
+
+
+def get_valid_location(board):  # to return the valid locations
+    valid_locations = []
+    for col in range(col_count):
+        if can_put(board, col):
+            valid_locations.append(col)
+    return valid_locations  # Return the columns in which move can be made
+
+
+def pick_best(board, piece):  # To choose the best column
+    best_score = -10000
+    valid_location = get_valid_location(board)
+    best_col = random.choice(
+        valid_location
+    )  # choose any valid lcation randomly initially
+    for col in valid_location:
+        row = next_top_row(
+            board, col
+        )  # get the row that can be filled for valid columns
+        temp_board = board.copy()  # making a copy of board
+        put_piece(temp_board, row, col, piece)
+        score = get_score(temp_board, piece)  # get the score of current arrangement
+        if score > best_score:  # if new best score is found update it
+            best_score = score
+            best_col = col
+    return best_col
+
+
 if __name__ == "__main__":
 
     board = board()  # A 6*7 matrix with all zeros
     game_over = False
-    turn = 0  # player 1 goes first
-
     pygame.init()  # initializing the pygame
     SQUARESIZE = 100  # 1 square sixe is 100 pixel
     RADIUS = int(SQUARESIZE / 2 - 5)  # Radius of the inner circle
@@ -150,8 +302,8 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode(size)  # The dimensions of the game area
     draw_board(board)
     pygame.display.update()  # To update the screen
-    my_font=pygame.font.SysFont("monospace",45)#Font of the final message
-
+    my_font = pygame.font.SysFont("monospace", 45)  # Font of the final message
+    turn = random.randint(0, 1)  # The turn is selected randomly
     while not game_over:
         for event in pygame.event.get():
             # Whenever a event like key press/mouse movement occurs
@@ -160,59 +312,66 @@ if __name__ == "__main__":
 
             if event.type == pygame.MOUSEMOTION:  # Whenever the mouse moves
                 # For the upper space
-                pygame.draw.rect(screen, (0,0,0), (0, 0, width, SQUARESIZE))
+                pygame.draw.rect(screen, (0, 0, 0), (0, 0, width, SQUARESIZE))
                 # masks previous colour
                 posx = event.pos[0]
                 if turn == 0:  # for player 1
                     pygame.draw.circle(
                         screen, (255, 255, 0), (posx, int(SQUARESIZE / 2)), RADIUS
                     )
-                else:  # for player 2
-                    pygame.draw.circle(
-                        screen, (255, 0, 0), (posx, int(SQUARESIZE / 2)), RADIUS
-                    )
             pygame.display.update()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # if   mouse is clicked
-                pygame.draw.rect(screen,(0,0,0),(0,0,width,SQUARESIZE))#Masks previous coloured circle
+                pygame.draw.rect(
+                    screen, (0, 0, 0), (0, 0, width, SQUARESIZE)
+                )  # Masks previous coloured circle
 
                 # player 1's turn
                 if turn == 0:
                     # col_move = int(input(" Player 1's turn "))  # User enters column values
                     posx = event.pos[0]  # position of x-axis of mouse click
                     col_move = int(math.floor(posx / SQUARESIZE))
+
                     if can_put(board, col_move):
                         row = next_top_row(board, col_move)
                         put_piece(board, row, col_move, 1)  # player 1's piece is 1
+
+                        if win(board, 1):
+                            label = my_font.render(
+                                "!!! Player 1 Wins !!!", 1, (255, 255, 0)
+                            )
+                            screen.blit(label, (40, 10))
+                            # The winning message
+                            game_over = True  # The game is over
+
+                    turn += 1
+                    turn = turn % 2  # Turn will be either  0 or 1
                     print_board(board)
                     draw_board(board)
 
-                    if win(board, 1):
-                        label=my_font.render("!!! Player 1 Wins !!!",1,(255,255,0))
-                        screen.blit(label,(40,10))
-                        # The winning message
-                        game_over = True  # The game is over
+        if turn == 1 and not game_over:  # AI's turn
+            # col_move = int(input(" AI's turn "))
+            # col_move = pick_best(board, 2)
+            col_move, minmax_score = minmax(
+                board, 3, -math.inf, math.inf, True
+            )  # The depth of 3 for maximizing player
+            # alpha==>Negative infinity
+            # beta==>positive infinity
+            if can_put(board, col_move):
+                pygame.time.wait(400)  # time before AI makes a move
+                row = next_top_row(board, col_move)
+                put_piece(board, row, col_move, 2)  # AI's piece is 2
 
-                else:  # Player 2's turn
-                    # col_move = int(input(" Player 2's turn "))
-                    posx = event.pos[0]  # position of x-axis of mouse click
-                    col_move = int(math.floor(posx / SQUARESIZE))
-                    if can_put(board, col_move):
-                        row = next_top_row(board, col_move)
-                        put_piece(board, row, col_move, 2)  # player 2's piece is 2
-                    print_board(board)
+            if win(board, 2):
+                label = my_font.render("!!! AI Wins !!!", 1, (255, 0, 0))
+                screen.blit(label, (40, 10))
+                # The winning message
+                game_over = True  # The game is over
 
-                    if win(board, 2):
-                        label=my_font.render("!!! Player 2 Wins !!!",1,(255,0,0))
-                        screen.blit(label,(40,10))
-                        # The winning message
-                        
-                        game_over = True  # The game is over
-
-                draw_board(board)
-                
-                turn += 1
-                turn = turn % 2  # Turn we either be 0 or 1
-                if game_over:
-                    pygame.time.wait(5000)#Wait before closing the window after game is over
+            turn += 1
+            turn = turn % 2  # Turn will be either  0 or 1
+            draw_board(board)
+            print_board(board)
+    if game_over:
+        pygame.time.wait(5000)  # Wait before closing the window after game is over
